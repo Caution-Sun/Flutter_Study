e99893bd16266430eb0063e40a5a4e6406c33c91
Caution-Sun <hisbrjseka414>
2024-03-01 16:11:33 +0900

    tabber_example

---
+++ tabbar_example/lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:tabbar_example/sub/firstPage.dart';
+import 'package:tabbar_example/sub/secondPage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
+  TabController? tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TabBar Example'),
+      ),
+      body: TabBarView(
+        children: <Widget>[FirstApp(), SecondApp()],
+        controller: tabController,
+      ),
+      bottomNavigationBar: TabBar(tabs: <Tab>[
+        Tab(icon: Icon(Icons.looks_one, color: Colors.blue,),),
+        Tab(icon: Icon(Icons.looks_two, color: Colors.blue,),)
+      ], controller: tabController,),
+    );
+  }
+
+  @override
+  void dispose() {
+    tabController!.dispose();
+    super.dispose();
+  }
+}
