e45c7af571050433a2789a500a1c7ec7631f9b7f
Caution-Sun <hisbrjseka414>
2024-03-13 16:08:41 +0900

    http_example_2

--- http_example/lib/main.dart
+++ http_example/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(const MyApp());
@@ -28,6 +29,13 @@ class HttpApp extends StatefulWidget {
 
 class _HttpApp extends State<HttpApp> {
   String result = '';
+  List? data;
+
+  @override
+  void initState(){
+    super.initState();
+    data = new List.empty(growable: true);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,19 +45,51 @@ class _HttpApp extends State<HttpApp> {
       ),
       body: Container(
         child: Center(
-          child: Text('$result'),
+          child: data!.length == 0
+              ? Text('데이터가 없습니다', style: TextStyle(fontSize: 20), textAlign: TextAlign.center,)
+              : ListView.builder(itemBuilder: (context, index) {
+                return Card(
+                  child: Container(
+                    child: Column(
+                      children: <Widget>[
+                        Text(data![index]['title'].toString()),
+                        Text(data![index]['authors'].toString()),
+                        Text(data![index]['sale_price'].toString()),
+                        Text(data![index]['status'].toString()),
+                        Image.network(
+                          data![index]['thumbnail'],
+                          height: 100,
+                          width: 100,
+                          fit: BoxFit.contain,
+                        )
+                      ],
+                    )
+                  )
+                );
+          },
+          itemCount: data!.length
+          )
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          var url = 'http://www.google.com';
-          var response = await http.get(Uri.parse(url));
-          setState(() {
-            result = response.body;
-          });
+        onPressed: () {
+          getJSONData();
         },
         child: Icon(Icons.file_download),
       ),
     );
+  }
+
+  Future<String> getJSONData() async {
+    var url = 'https://dapi.kakao.com/v3/search/book?target=title&query=doit';
+    var response = await http.get(Uri.parse(url), headers: {"Authorization": "KakaoAK ##REST API KEY 입력 부분##"});
+
+    setState(() {
+      var dataConvertedToJSON = json.decode(response.body);
+      List result = dataConvertedToJSON['documents'];
+      data!.addAll(result);
+    });
+
+    return response.body;
   }
 }
