df2a8d03538b0c66729a4a541365b9b46b532599
Caution-Sun <hisbrjseka414>
2024-03-14 15:33:07 +0900

    http_example_3

--- http_example/lib/main.dart
+++ http_example/lib/main.dart
@@ -30,49 +30,84 @@ class HttpApp extends StatefulWidget {
 class _HttpApp extends State<HttpApp> {
   String result = '';
   List? data;
+  TextEditingController? _editingController;
+  int page = 1;
+  ScrollController? _scrollController;
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     data = new List.empty(growable: true);
+    _editingController = new TextEditingController();
+    _scrollController = new ScrollController();
+
+    _scrollController!.addListener(() {
+      if (_scrollController!.offset >=
+              _scrollController!.position.maxScrollExtent &&
+          !_scrollController!.position.outOfRange) {
+        print('bottom');
+        page++;
+        getJSONData();
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Http Example'),
-      ),
+          title: TextField(
+        controller: _editingController,
+        style: TextStyle(color: Colors.black),
+        keyboardType: TextInputType.text,
+        decoration: InputDecoration(hintText: '검색어를 입력하세요'),
+      )),
       body: Container(
         child: Center(
-          child: data!.length == 0
-              ? Text('데이터가 없습니다', style: TextStyle(fontSize: 20), textAlign: TextAlign.center,)
-              : ListView.builder(itemBuilder: (context, index) {
-                return Card(
-                  child: Container(
-                    child: Column(
-                      children: <Widget>[
-                        Text(data![index]['title'].toString()),
-                        Text(data![index]['authors'].toString()),
-                        Text(data![index]['sale_price'].toString()),
-                        Text(data![index]['status'].toString()),
-                        Image.network(
-                          data![index]['thumbnail'],
-                          height: 100,
-                          width: 100,
-                          fit: BoxFit.contain,
-                        )
-                      ],
-                    )
+            child: data!.length == 0
+                ? Text(
+                    '데이터가 없습니다',
+                    style: TextStyle(fontSize: 20),
+                    textAlign: TextAlign.center,
                   )
-                );
-          },
-          itemCount: data!.length
-          )
-        ),
+                : ListView.builder(
+                    itemBuilder: (context, index) {
+                      return Card(
+                          child: Container(
+                              child: Row(
+                        children: <Widget>[
+                          Image.network(
+                            data![index]['thumbnail'],
+                            height: 100,
+                            width: 100,
+                            fit: BoxFit.contain,
+                          ),
+                          Column(
+                            children: <Widget>[
+                              Container(
+                                width: MediaQuery.of(context).size.width - 150,
+                                child: Text(
+                                  data![index]['title'].toString(),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                              Text('저자 : ${data![index]['authors'].toString()}'),
+                              Text(
+                                  '가격 : ${data![index]['sale_price'].toString()}'),
+                              Text('판매중 : ${data![index]['status'].toString()}')
+                            ],
+                          )
+                        ],
+                        mainAxisAlignment: MainAxisAlignment.start,
+                      )));
+                    },
+                    itemCount: data!.length,
+                    controller: _scrollController)),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
+          page = 1;
+          data!.clear();
           getJSONData();
         },
         child: Icon(Icons.file_download),
@@ -81,8 +116,10 @@ class _HttpApp extends State<HttpApp> {
   }
 
   Future<String> getJSONData() async {
-    var url = 'https://dapi.kakao.com/v3/search/book?target=title&query=doit';
-    var response = await http.get(Uri.parse(url), headers: {"Authorization": "KakaoAK ##REST API KEY 입력 부분##"});
+    var url =
+        'https://dapi.kakao.com/v3/search/book?target=title&page=$page&query=${_editingController!.value.text}';
+    var response = await http.get(Uri.parse(url),
+        headers: {"Authorization": "KakaoAK ##REST API KEY##"});
 
     setState(() {
       var dataConvertedToJSON = json.decode(response.body);
