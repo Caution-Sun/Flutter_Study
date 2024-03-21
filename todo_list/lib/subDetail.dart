df500518b7200f8a8d0537350338a758a7e276c3
Caution-Sun <hisbrjseka414>
2024-03-21 16:28:22 +0900

    navigation(3)

--- todo_list/lib/subDetail.dart
+++ todo_list/lib/subDetail.dart
@@ -7,21 +7,56 @@ class SubDetail extends StatefulWidget {
   }
 }
 
-class _SubDetail extends State<SubDetail>{
+class _SubDetail extends State<SubDetail> {
+  List<String> todoList = new List.empty(growable: true);
+
+  @override
+  void initState() {
+    super.initState();
+
+    todoList.add(('Flutter 공부하기'));
+    todoList.add(('코딩 테스트 풀기'));
+    todoList.add(('면접 예상질문 추가하기'));
+    todoList.add(('면접 예상질문 복습하기'));
+    todoList.add(('앱 개발하기'));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Sub Detail Example'),),
-      body: Container(
-        child: Center(
-          child: ElevatedButton(
-            onPressed: (){
-              Navigator.of(context).pushNamed('/second');
-            },
-            child: Text('두 번째 페이지로 이동하기'),
-          ),
-        ),
+      appBar: AppBar(
+        title: Text('Sub Detail Example'),
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return Card(
+            child: InkWell(
+              child: Text(
+                todoList[index],
+                style: TextStyle(fontSize: 30),
+              ),
+              onTap: () {
+                Navigator.of(context)
+                    .pushNamed('/third', arguments: todoList[index]);
+              },
+            ),
+          );
+        },
+        itemCount: todoList.length,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _addNavigation(context);
+        },
+        child: Icon(Icons.add),
       ),
     );
+  }
+
+  void _addNavigation(BuildContext context) async {
+    final result = await Navigator.of(context).pushNamed('/second');
+    setState(() {
+      todoList.add(result as String);
+    });
   }
 }
