c479e68e16ed458d4d5ade810ab8a32eb086b81e
Caution-Sun <hisbrjseka414>
2024-03-28 16:41:09 +0900

    File data_2

--- fileio_example/lib/fileApp.dart
+++ fileio_example/lib/fileApp.dart
@@ -1,23 +1,27 @@
 import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class FileApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return _FileApp();
   }
-
 }
 
-class _FileApp extends State<FileApp>{
+class _FileApp extends State<FileApp> {
   int _count = 0;
+  List<String> itemList = new List.empty(growable: true);
+  TextEditingController controller = new TextEditingController();
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
 
     readCountFile();
+
+    initData();
   }
 
   @override
@@ -28,19 +32,46 @@ class _FileApp extends State<FileApp>{
       ),
       body: Container(
         child: Center(
-          child: Text('$_count', style: TextStyle(fontSize: 40),),
+          child: Column(
+            children: <Widget>[
+              TextField(
+                controller: controller,
+                keyboardType: TextInputType.text,
+              ),
+              Expanded(
+                  child: ListView.builder(
+                      itemBuilder: (context, index) {
+                        return Card(
+                          child: Center(
+                            child: Text(
+                              itemList[index],
+                              style: TextStyle(fontSize: 30),
+                            ),
+                          ),
+                        );
+                      },
+                      itemCount: itemList.length))
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
+          writeFruit(controller.value.toString());
           setState(() {
-            _count++;
+            itemList.add(controller.value.text);
           });
-          writeCountFile(_count);
         },
         child: Icon(Icons.add),
       ),
     );
+  }
+
+  void initData() async {
+    var result = await readListFile();
+    setState(() {
+      itemList.addAll(result);
+    });
   }
 
   void writeCountFile(int count) async {
@@ -49,7 +80,7 @@ class _FileApp extends State<FileApp>{
   }
 
   void readCountFile() async {
-    try{
+    try {
       var dir = await getApplicationDocumentsDirectory();
       var file = await File(dir.path + '/count.txt').readAsString();
       print(file);
@@ -57,10 +88,48 @@ class _FileApp extends State<FileApp>{
       setState(() {
         _count = int.parse(file);
       });
-
-    }catch(e){
+    } catch (e) {
       print(e.toString());
     }
   }
 
+  Future<List<String>> readListFile() async {
+    List<String> itemList = new List.empty(growable: true);
+    var key = 'first';
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    bool? firstCheck = pref.getBool(key);
+    var dir = await getApplicationDocumentsDirectory();
+    bool firstExist = await File(dir.path + '/fruit.txt').exists();
+
+    if (firstCheck == null || firstCheck == false || firstExist == false) {
+      pref.setBool(key, true);
+
+      var file =
+          await DefaultAssetBundle.of(context).loadString('repo/fruit.txt');
+      File(dir.path + '/fruit.txt').writeAsStringSync(file);
+      var array = file.split('\n');
+      for (var item in array) {
+        print(item);
+        itemList.add(item);
+      }
+
+      return itemList;
+    } else {
+      var file = await File(dir.path + '/fruit.txt').readAsString();
+      var array = file.split('\n');
+      for (var item in array) {
+        print(item);
+        itemList.add(item);
+      }
+
+      return itemList;
+    }
+  }
+
+  void writeFruit(String fruit) async {
+    var dir = await getApplicationDocumentsDirectory();
+    var file = await File(dir.path + '/fruit.txt').readAsString();
+    file = file + '\n' + fruit;
+    File(dir.path + '/fruit.txt').writeAsStringSync(file);
+  }
 }
